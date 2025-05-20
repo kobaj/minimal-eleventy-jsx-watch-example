12546b7a10d4a325d074625bccd2262c9f882c33
Kobaj <kobaj.g@gmail.com>
2025-05-20 16:20:29 -0700

    Setup eleventy with TSX.

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+import "tsx/esm";
+
+import { jsxToString } from "jsx-async-runtime";
+
+export default async function (eleventyConfig) {
+  eleventyConfig.addExtension(["11ty.jsx", "11ty.ts", "11ty.tsx"], {
+    key: "11ty.js",
+    compile: async function (inputContent, inputPath) {
+      return async function (data) {
+        let content = await this.defaultRenderer(data);
+        return jsxToString(content);
+      };
+    },
+  });
+
+  return {
+    templateFormats: [
+      "11ty.jsx",
+      "11ty.tsx",
+    ],
+
+    useTemplateCache: false,
+
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "dist"
+    }
+  };
+};
